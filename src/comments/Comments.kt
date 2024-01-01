0823f672141718c930e1c6729fa453ccf14514e6
nyadero <briannyadero443@gmail.com>
2024-01-01 18:00:57 +0300

    comments in kotlin

---
+++ src/comments/Comments.kt
@@ -0,0 +1,13 @@
+package comments
+
+fun main() {
+    println("Single line comment")
+//    single line comment
+
+    /*
+    this is an example of multiline comment
+    Another comment can follow here
+    ................................
+    _______________________________________
+     */
+}
