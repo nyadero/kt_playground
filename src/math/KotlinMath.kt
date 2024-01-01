ab0ace57de35d26e7eda659a401a30e5d96df0f7
nyadero <briannyadero443@gmail.com>
2024-01-01 18:11:38 +0300

    kotlin math functions

---
+++ src/math/KotlinMath.kt
@@ -0,0 +1,18 @@
+package math
+
+import kotlin.math.*
+
+fun main() {
+//    get value of pi
+    println("Value of Pi: $PI")
+//    print largest number
+    println("Largest: ${max(1000, 100)}")
+//    print smallest number
+    println("Smallest:  ${min(1000, 100)}")
+//    square root
+    println("Square root: ${sqrt(5.0)}")
+//    round off
+    println("Round up: ${ceil(5.7)}")
+//    round down
+    println("Round down: ${floor(5.7)}")
+}
