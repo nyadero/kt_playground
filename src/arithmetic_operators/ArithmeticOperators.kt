0b8742fc672f82b369150890abdee51260b8d54d
nyadero <briannyadero443@gmail.com>
2024-01-01 18:18:50 +0300

    basic arithmetic operations in kotlin

---
+++ src/arithmetic_operators/ArithmeticOperators.kt
@@ -0,0 +1,21 @@
+package arithmetic_operators
+
+fun main() {
+    var number1: Int = 10
+    val number2: Int = 40
+
+//    addition
+    println("Additions : ${number1 + number2}")
+//    subtraction
+    println("Subtraction: ${number2 - number1}")
+//    division
+    println("Division: ${number2 / number1}")
+//    multiplication
+    println("Multiplication: ${number1 * number2}")
+//    modulus
+    println("Modulus: ${number2%number1}")
+//    increase number by 1
+    println(++number1)
+    println(--number1)
+
+}
